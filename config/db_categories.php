4a1b6f853fa258ddacc4337e36591978317ee0b3
danieldorazio <danieldorazio.97@gmail.com>
2024-02-16 19:18:08 +0100

    add: file seeder

---
+++ config/db_categories.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+    [
+      "id" => 1,
+      "name" => "italiano",
+      "slug" => "italiano",
+      "image_categoty" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+      "image_header" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+    ],
+    [
+      "id" => 2,
+      "name" => "pizza",
+      "slug" => "pizza",
+      "image_categoty" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+      "image_header" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+    ],
+    [
+      "id" => 3,
+      "name" => "cinese",
+      "slug" => "cinese",
+      "image_categoty" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+      "image_header" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+    ],
+    [
+      "id" => 4,
+      "name" => "giapponese",
+      "slug" => "giapponese",
+      "image_categoty" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+      "image_header" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+    ],
+    [
+      "id" => 5,
+      "name" => "greco",
+      "slug" => "greco",
+      "image_categoty" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+      "image_header" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+    ],
+    [
+      "id" => 6,
+      "name" => "poke",
+      "slug" => "poke",
+      "image_categoty" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+      "image_header" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+    ],
+    [
+      "id" => 7,
+      "name" => "messicano",
+      "slug" => "messicano",
+      "image_categoty" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+      "image_header" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+    ],
+    [
+      "id" => 8,
+      "name" => "hamburgher",
+      "slug" => "hamburgher",
+      "image_categoty" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+      "image_header" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+    ],
+    [
+      "id" => 9,
+      "name" => "indiano",
+      "slug" => "indiano",
+      "image_categoty" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+      "image_header" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+    ],
+    [
+      "id" => 10,
+      "name" => "dolce",
+      "slug" => "dolce",
+      "image_categoty" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+      "image_header" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/79/47/35/tien-tsin.jpg?w=600&h=400&s=1",
+    ],
+];
