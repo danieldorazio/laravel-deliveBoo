4161eeb0d7908ef85718e4ea6b1ef252f437ab94
danieldorazio <danieldorazio.97@gmail.com>
2024-02-19 16:21:45 +0100

    implemented image storage link

--- resources/views/admin/meals/show.blade.php
+++ resources/views/admin/meals/show.blade.php
@@ -2,37 +2,42 @@
 
 
 @section('content')
-<div class="container mt-5">
+    <div class="container mt-5">
 
-    @include('partials.previous_button')
+        @include('partials.previous_button')
 
-    
-    <h2><strong>Name : </strong>{{ $meal->name}}</h2>
 
-    <div class="mt-4 w-50">
-        <p>
-            <strong>Image : </strong>{{ $meal->image ?? 'Not available ' }}
-        </p>
+        <h2><strong>Name : </strong>{{ $meal->name }}</h2>
+
+        <div class="mt-4 w-50">
+            @if ($meal->image)
+                <div>
+                    <img src="{{ asset('storage/' . $meal->image) }}" alt="">
+                </div>
+            @else
+                <p>
+                    <strong>Image : Not available </strong>
+                </p>
+            @endif
+        </div>
+
+        <div class="mt-4 w-50">
+            <p>
+                <strong>Slug : </strong>{{ $meal->slug }}
+            </p>
+        </div>
+
+        <div class="mt-4 w-50">
+            <p>
+                <strong>Description : </strong>{{ $meal->description ?? 'Not available ' }}
+            </p>
+        </div>
+
+        <div class="mt-4 w-50">
+            <p>
+                <strong>Price : </strong>{{ $meal->price ?? 'Not available ' }}
+            </p>
+        </div>
+
     </div>
-
-    <div class="mt-4 w-50">
-        <p>
-            <strong>Slug : </strong>{{ $meal->slug}}
-        </p>
-    </div>
-
-    <div class="mt-4 w-50">
-        <p>
-            <strong>Description : </strong>{{ $meal->description ?? 'Not available ' }}
-        </p>
-    </div>
-
-    <div class="mt-4 w-50">
-        <p>
-            <strong>Price : </strong>{{ $meal->price ?? 'Not available ' }}
-        </p>
-    </div>
-
-</div>
-
 @endsection
