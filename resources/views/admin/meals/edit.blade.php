31db15249a0fdb6783154422d7b34b534e2a5572
MarcoGranata9 <granatam9900@gmail.com>
2024-02-19 11:05:32 +0100

    add: edit meals page

--- resources/views/admin/meals/edit.blade.php
+++ resources/views/admin/meals/edit.blade.php
@@ -2,19 +2,19 @@
 
 <div class="container">
     <div class="d-flex align-items-center gap-5 mt-5">
-        <h1>Nuovo Piatto:</h1>
+        <h1>Modifica Piatto:</h1>
     </div>
 </div>
 
 <div class="row justify-content-center mt-5">
     <div class="col-6">
 
-        <form action="{{ route('admin.meals.store') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('admin.meals.update', ['meals' => $meals->slug]) }}" method="POST" enctype="multipart/form-data">
             @csrf
             
             {{-- Nome Piatto --}}
             <div class="form-floating mb-3">
-                <input placeholder="name@example.com" type="text" class="form-control @error('name') is-invalid  @enderror"  id="name" name="name" value="{{ old('name') }}">
+                <input placeholder="name@example.com" type="text" class="form-control @error('name') is-invalid  @enderror"  id="name" name="name" value="{{ old('name'), $meals->name }}">
                 <label for="name">Nome</label>
                 @error('name')
                     <div class="invalid-feedback">{{ $message }}</div>
@@ -23,7 +23,7 @@
             
             {{-- Descrizione Piatto --}}
             <div class="form-floating mb-3">
-                <textarea class="form-control" placeholder="Descrizione piatto" id="description" style="height: 100px" name="description">{{ old('description') }}</textarea>
+                <textarea class="form-control" placeholder="Descrizione piatto" id="description" style="height: 100px" name="description">{{ old('description'), $project->description }}</textarea>
                 <label for="description">Descrizione</label>
             </div>
 
@@ -38,7 +38,7 @@
             
             {{-- Prezzo Piatto --}}
             <div>
-                <input placeholder="name@example.com" type="text" class="form-control @error('price') is-invalid  @enderror"  id="price" name="price" value="{{ old('price') }}">
+                <input placeholder="name@example.com" type="text" class="form-control @error('price') is-invalid  @enderror"  id="price" name="price" value="{{ old('price'), $meals->price }}">
                 <label for="price">Nome</label>
                 @error('price')
                     <div class="invalid-feedback">{{ $message }}</div>
@@ -47,7 +47,7 @@
 
             {{-- Piatto Disponibile --}}
             <div>
-                <input class="form-check-input" type="checkbox" value="{{ old('available')}}" id="available" name="available">
+                <input class="form-check-input" type="checkbox" value="{{ old('available'), $meals->available }}" id="available" name="available">
                 <label class="form-check-label" for="available">
                     Piatto Disponibile
                 </label>
