528fd9b49934bf9db735ba0c2fde36183ae94771
danieldorazio <danieldorazio.97@gmail.com>
2024-02-23 23:30:50 +0100

    fixed:db_category with the img

--- config/db_categories.php
+++ config/db_categories.php
@@ -4,41 +4,49 @@ return [
     [
       "id"             => 1,
       "name"           => "Italian",
+      "image_categoty" => "category_image_categoty/italian.png",
+      "image_header"   => "category_image_header/italian.png",
     ],
     [
       "id"             => 2,
       "name"           => "Pizza",
+      "image_categoty" => "category_image_categoty/pizza.png",
+      "image_header"   => "category_image_header/pizza.png",
     ],
     [
       "id"             => 3,
-      "name"           => "Chinese",
+      "name"           => "Japanese",
+      "image_categoty" => "category_image_categoty/japanese.png",
+      "image_header"   => "category_image_header/japanese.png",
     ],
     [
       "id"             => 4,
-      "name"           => "Japanese",
+      "name"           => "Poke",
+      "image_categoty" => "category_image_categoty/poke.png",
+      "image_header"   => "category_image_header/poke.png",
     ],
     [
       "id"             => 5,
-      "name"           => "Greek",
+      "name"           => "Mexican",
+      "image_categoty" => "category_image_categoty/mexican.png",
+      "image_header"   => "category_image_header/mexican.png",
     ],
     [
       "id"             => 6,
-      "name"           => "Poke",
+      "name"           => "Hamburger",
+      "image_categoty" => "category_image_categoty/hamburger.png",
+      "image_header"   => "category_image_header/hamburger.png",
     ],
     [
       "id"             => 7,
-      "name"           => "Mexican",
+      "name"           => "Indian",
+      "image_categoty" => "category_image_categoty/indian.png",
+      "image_header"   => "category_image_header/indian.png",
     ],
     [
       "id"             => 8,
-      "name"           => "Hamburger",
-    ],
-    [
-      "id"             => 9,
-      "name"           => "Indian",
-    ],
-    [
-      "id"             => 10,
       "name"           => "Dessert",
+      "image_categoty" => "category_image_categoty/dessert.png",
+      "image_header"   => "category_image_header/dessert.png",
     ],
 ];
