e0c4a41b3c749d5e5f861938f29502339b7b06eb
danieldorazio <danieldorazio.97@gmail.com>
2024-02-26 12:13:19 +0100

    create category_restaurant seeder

---
+++ config/db_category_restaurant.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    [
+        "category_id"   => 1,
+        "restaurant_id" => 1,
+    ],
+    [
+        "category_id"   => 2,
+        "restaurant_id" => 7,
+    ],
+    [
+        "category_id"   => 3,
+        "restaurant_id" => 3,
+    ],
+    [
+        "category_id"   => 4,
+        "restaurant_id" => 9,
+    ],
+    [
+        "category_id"   => 5,
+        "restaurant_id" => 4,
+    ],
+    [
+        "category_id"   => 6,
+        "restaurant_id" => 8,
+    ],
+    [
+        "category_id"   => 7,
+        "restaurant_id" => 6,
+    ],
+    [
+        "category_id"   => 8,
+        "restaurant_id" => 10,
+    ],
+];
