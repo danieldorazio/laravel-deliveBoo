9743c3b54b2e86d158da952674e9f3d186979ac0
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2024-02-29 09:57:06 +0100

    update: ho cambiato il nome della pagina, se non funzia, toglietelo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>DeliveBoo</title>
 
     <!-- Fontawesome 6 cdn -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
